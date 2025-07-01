83bfd5d8063a80008c19dcf49e930dfc8a131122
Sviatoslav Murzin <mursvyats@gmail.com>
2025-07-02 03:57:54 +0500

    feat: added memoization

--- dist/main.compiled.js
+++ dist/main.compiled.js
@@ -58,7 +58,7 @@ function Header() {
     href: "/scripts"
   }, "\u0421\u0446\u0435\u043D\u0430\u0440\u0438\u0438"))));
 }
-function Event(props) {
+var Event = React.memo(function EventComponent(props) {
   var ref = React.useRef();
   var onSize = props.onSize;
   React.useEffect(function () {
@@ -85,7 +85,7 @@ function Event(props) {
   }, props.title), props.subtitle && /*#__PURE__*/React.createElement("span", {
     className: "event__subtitle"
   }, props.subtitle)));
-}
+});
 var TABS = {
   "all": {
     "title": "Все",
